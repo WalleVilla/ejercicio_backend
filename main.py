f592cdab4e1bbd5fcb38e7bd2c69c80ba1b25e6a
Hanzeel <hanzeelvilla0@gmail.com>
2023-07-28 12:00:10 -0600

    Código READ by ID

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.sqltypes import TIMESTAMP #Aunque es similar a DateTime
 from sqlalchemy.orm import relationship
@@ -199,6 +199,63 @@ def get_purchase_order_detail():
     result = db.session.execute(db.select(Purchase_order_detail).order_by(Purchase_order_detail.purchase_order_id))
     all_results = result.scalars().all()
     return jsonify(purchase_order_detail=[purchase.to_dict() for purchase in all_results])
+
+# -------------------------------- READ BY ID -------------------------------- #
+
+#CATEGORIES
+@app.route("/search-category")
+def search_category():
+    id = request.args.get("id")
+    result = db.session.execute(db.select(Categories).where(Categories.id == id))
+    all_results = result.scalars().all()
+    if all_results:
+        return jsonify(category=[cat.to_dict() for cat in all_results])
+    else:
+        return jsonify(error={"Not Found": "The id doesn't exist"}), 404
+
+#PRODUCTS
+@app.route("/search-product")
+def search_product():
+    id = request.args.get("id")
+    result = db.session.execute(db.select(Products).where(Products.id == id))
+    all_results = result.scalars().all()
+    if all_results:
+        return jsonify(product=[product.to_dict() for product in all_results])
+    else:
+        return jsonify(error={"Not Found": "The id doesn't exist"}), 404
+
+#PROVIDERS
+@app.route("/search-provider")
+def search_provider():
+    id = request.args.get("id")
+    result = db.session.execute(db.select(Providers).where(Providers.id == id))
+    all_results = result.scalars().all()
+    if all_results:
+        return jsonify(provider=[provider.to_dict() for provider in all_results])
+    else:
+        return jsonify(error={"Not Found": "The id doesn't exist"}), 404
+
+#PURCHASE ORDERS
+@app.route("/search-purchase-order")
+def search_purchase_order():
+    id = request.args.get("id")
+    result = db.session.execute(db.select(Purchase_orders).where(Purchase_orders.provider_id == id))
+    all_results = result.scalars().all()
+    if all_results:
+        return jsonify(order=[order.to_dict() for order in all_results])
+    else:
+        return jsonify(error={"Not Found": "The id doesn't exist"}), 404
+    
+#PURCHASE ORDER DETAIL
+@app.route("/search-purchase-order-detail")
+def search_purchase_order_detail():
+    id = request.args.get("id")
+    result = db.session.execute(db.select(Purchase_order_detail).where(Purchase_order_detail.purchase_order_id == id))
+    all_results = result.scalars().all()
+    if all_results:
+        return jsonify(order=[order.to_dict() for order in all_results])
+    else:
+        return jsonify(error={"Not Found": "The id doesn't exist"}), 404
 
 # --------------------------------- HOME PAGE -------------------------------- #
  
